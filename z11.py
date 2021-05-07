86bf92c8a51f4191f902724830ff922893538eeb
Maryna Kulish <maryna.kulish@tietoevry.com>
2021-05-07 11:48:40 +0200

    zadanie 1 poprawione

--- z11.py
+++ z11.py
@@ -12,5 +12,8 @@ with open("z1.txt", 'r') as input_file:
     word_count = {word: word_list.count(word) for word in word_list}
     print(word_count)
     print('Największa ilość wystąpień:')
-    print(sorted(word_count.items(), key=lambda o: o[1], reverse=True)[:5])
-    print('Lączna licba slow w tekscie: ', len(file_contents.split()))
+    x = sorted(word_count.items(), key=lambda o: o[1], reverse=True)[:5]
+    for k,o in reversed(x):
+        print(str(k)+": "+str(o))
+
+    print('Lączna licba slow w tekscie: ', len(word_list))
